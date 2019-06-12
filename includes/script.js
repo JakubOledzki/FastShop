7f218cb0a1d48ac25c636cec57241b391b97b723
JakubOledzki <jakuboledzki21@gmail.com>
2019-06-12 23:41:35 +0200

    update

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-    $('#phone').fadeOut(0, function() {
-     $('#phone').fadeIn(1000);
-    });
+    
+    $('#phone').css("visibility","hidden");
+    setTimeout(function(){
+        $('#phone').css("visibility","visible");
+        $('#phone').fadeOut(0, function() {
+            $('#phone').fadeIn(1000);
+        });    
+    },2000);
     
     /*$("#navhome").click(function(){
         $('html, body').animate({
