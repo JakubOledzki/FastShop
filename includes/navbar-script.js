90a3919aeb1f70c9714beff6764707dd4437462a
JakubOledzki <jakuboledzki21@gmail.com>
2019-06-12 16:10:51 +0200

    Update

--- includes/navbar-script.js
+++ includes/navbar-script.js
@@ -1,19 +1,24 @@
 function checkPositionScroll(){
     
     var nav = document.querySelector(".navbar-own");
-    var navHeight = nav.style.height;
+    var container = document.querySelector(".container-fluid");
+    var navHeight = nav.offsetHeight;
+    var containerWidth = container.offsetWidth;
     
-    console.log(document.documentElement.scrollTop);
+    //console.log(document.documentElement.scrollTop);
+    //console.log(navHeight);
     
-    if(document.documentElement.scrollTop >= 200){
-        nav.style.backgroundColor = "pink";
-        nav.style.position = "sticky";
+    if(document.documentElement.scrollTop >= navHeight+10){
+        nav.style.backgroundColor = "#000";
+        nav.style.color = "#f8f9fa";
+        nav.style.position = "fixed";
         nav.style.top = "0";
         nav.style.left = "0";
+        nav.style.width = containerWidth;
     }
     else if (document.documentElement.scrollTop === 0){
-        nav.style.background = "transparent";
-        nav.style.position = "relative";
+        nav.style.background = "rgba(0, 0, 0, 0.10)";
+        nav.style.position = "absolute";
     }
 }
 window.addEventListener("scroll", checkPositionScroll);
