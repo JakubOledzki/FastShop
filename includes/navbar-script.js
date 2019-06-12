fe2c194bf6b47647a797c8947b1adc6f10a654fd
JakubOledzki <jakuboledzki21@gmail.com>
2019-06-12 15:39:19 +0200

    scroll fix

--- includes/navbar-script.js
+++ includes/navbar-script.js
@@ -1,12 +1,15 @@
 function checkPositionScroll(){
     
     var nav = document.querySelector(".navbar-own");
+    var navHeight = nav.style.height;
     
     console.log(document.documentElement.scrollTop);
     
-    if(document.documentElement.scrollTop >= 100){
-        nav.style.backgroundColor = "#000";
+    if(document.documentElement.scrollTop >= 200){
+        nav.style.backgroundColor = "pink";
         nav.style.position = "sticky";
+        nav.style.top = "0";
+        nav.style.left = "0";
     }
     else if (document.documentElement.scrollTop === 0){
         nav.style.background = "transparent";
