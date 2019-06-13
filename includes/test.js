442654ef77711161eb668bd4bfac0ea7bcc1ce49
JakubOledzki <jakuboledzki21@gmail.com>
2019-06-13 14:03:35 +0200

    UPDATE

--- includes/test.js
+++ includes/test.js
@@ -1,8 +1,7 @@
 $(window).on('load', function(){
     
-    setTimeout(
-        function() {
+    
             $("#loader-wrapper").fadeOut();
             $(".js-loading").css("-webkit-animation-play-state", "running");
-         }, 3000);
+         
 });
